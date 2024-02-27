c50cbc3975091e5ebe6f9b85535a30a115193630
Muhammad Turmudzi <muhammad.turmudzi@mncgroup.com>
2024-02-27 22:13:42 +0700

    add plugin vee validate, yup and button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,5 +2,12 @@
 </script>
 
 <template>
-  <section>Home</section>
+  <section>
+    <h3>Home</h3>
+
+    <div>
+      <button class="btn--primary-outline">Reset</button>
+      <button class="btn--primary">Submit</button>
+    </div>
+  </section>
 </template>
