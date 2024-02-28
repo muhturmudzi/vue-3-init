f95aa4e23c929c80e1ca0226a57d7d66225a6607
Muhammad Turmudzi <muhammad.turmudzi@mncgroup.com>
2024-02-28 10:02:24 +0700

    add input dropdown

---
+++ src/components/form/InputDropDown.vue
@@ -0,0 +1,105 @@
+<script setup>
+import { computed, ref, watch } from 'vue'
+import { onClickOutside } from '@vueuse/core'
+
+const props = defineProps({
+  placeholder: {
+    type: String,
+    default: 'Select data'
+  },
+  modelValue: {
+    type: [String, Number],
+    default: ''
+  },
+  list: {
+    type: Array,
+    default: () => []
+  },
+  label: {
+    type: String,
+    default: 'name'
+  },
+  reduce: {
+    type: String,
+    default: 'id'
+  }
+})
+
+const search = ref('')
+const showList = ref(false)
+const target = ref(null)
+
+const emit = defineEmits(['update:modelValue'])
+
+watch(
+  () => props.modelValue,
+  (val) => {
+    console.log(val, 'model value')
+    const data = props.list.find(item => item[props.reduce] === val)
+    console.log(data, 'data')
+    if (!data) {
+      search.value = ''
+      return
+    }
+    search.value = data[props.label]
+  }
+)
+
+const filterList = computed(() =>
+  props.list.filter(item => {
+    return item[props.label].toLowerCase().includes(search.value.toLowerCase())
+  })
+)
+
+const handleSelect = (item) => {
+  console.log('select')
+  search.value = item[props.label]
+  emit('update:modelValue', item[props.reduce])
+  showList.value = false
+}
+
+const focus = (event) => {
+  console.log('focus')
+  showList.value = true
+}
+
+const focusOut = (event) => {
+  console.log('focus out')
+  setTimeout(() => {
+    showList.value = false
+  }, 100)
+}
+
+const handleKeyUp = (event) => {
+  console.log('onkeyup')
+  emit('update:modelValue', '')
+}
+
+onClickOutside(target, () => (showList.value = false))
+</script>
+
+<template>
+  <div ref="target" class="relative">
+    <input
+      class="input"
+      :placeholder="placeholder"
+      v-model="search"
+      @focus="focus"
+      @blur="focusOut"
+      @keyup="handleKeyUp"
+    />
+
+    <ul class="list-dropdown" :class="{'active': showList}">
+      <li
+        v-for="(item, index) in filterList"
+        :key="`${item[props.reduce]}-${index}`"
+        @click="handleSelect(item)"
+      >
+        {{ item[props.label] }}
+      </li>
+      <li v-if="filterList.length < 1">
+        Data not found
+      </li>
+    </ul>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,129 @@
 <script setup>
+import { ref } from 'vue'
+import InputDropDown from '@/components/form/InputDropDown.vue'
+
+const listData = [
+  {
+      "id": 1,
+      "name": "PKB",
+      "full_name": "Partai Kebangkitan Bangsa",
+  },
+  {
+      "id": 2,
+      "name": "Gerindra",
+      "full_name": "Partai Gerakan Indonesia Raya",
+  },
+  {
+      "id": 3,
+      "name": "PDI Perjuangan",
+      "full_name": "Partai Demokrasi Indonesia Perjuangan",
+  },
+  {
+      "id": 4,
+      "name": "Golkar",
+      "full_name": "Partai Golongan Karya",
+  },
+  {
+      "id": 5,
+      "name": "NasDem",
+      "full_name": "Partai NasDem",
+  },
+  {
+      "id": 6,
+      "name": "Partai Buruh",
+      "full_name": "Partai Buruh",
+  },
+  {
+      "id": 7,
+      "name": "Gelora",
+      "full_name": "Partai Gelombang Rakyat Indonesia",
+  },
+  {
+      "id": 8,
+      "name": "PKS",
+      "full_name": "Partai Keadilan Sejahtera",
+  },
+  {
+      "id": 9,
+      "name": "PKN",
+      "full_name": "Partai Kebangkitan Nusantara",
+  },
+  {
+      "id": 10,
+      "name": "Hanura",
+      "full_name": "Partai Hati Nurani Rakyat",
+  },
+  {
+      "id": 11,
+      "name": "Garuda",
+      "full_name": "Partai Garda Perubahan Indonesia",
+  },
+  {
+      "id": 12,
+      "name": "PAN",
+      "full_name": "Partai Amanat Nasional",
+  },
+  {
+      "id": 13,
+      "name": "PBB",
+      "full_name": "Partai Bulan Bintang",
+  },
+  {
+      "id": 14,
+      "name": "Partai Demokrat",
+      "full_name": "Partai Demokrat",
+  },
+  {
+      "id": 15,
+      "name": "PSI",
+      "full_name": "Partai Solidaritas Indonesia",
+  },
+  {
+      "id": 16,
+      "name": "Perindo",
+      "full_name": "Partai Persatuan Indonesia",
+  },
+  {
+      "id": 17,
+      "name": "PPP",
+      "full_name": "Partai Persatuan Pembangunan",
+  },
+  {
+      "id": 24,
+      "name": "Partai Ummat",
+      "full_name": "Partai Ummat",
+  }
+]
+
+const idPartai = ref('')
+
+const reset = () => {
+  idPartai.value = ''
+}
+
+const inject = (val) => {
+  idPartai.value = val
+}
 </script>
 
 <template>
   <section>
-    <h3>Home</h3>
+    <h3 class="mb-8 text-center">Vue's Components</h3>
 
-    <div>
-      <button class="btn--primary-outline">Reset</button>
-      <button class="btn--primary">Submit</button>
+    <div class="[&>*:not(:last-of-type)]:mr-4 mb-8">
+      <button class="btn--primary-outline" @click="reset">Reset</button>
+      <button class="btn--primary" @click="inject(4)">Inject Data</button>
     </div>
+
+    <InputDropDown
+      placeholder="Select Partai"
+      :list="listData"
+      label="name"
+      reduce="id"
+      v-model="idPartai"
+      class="mb-4"
+    />
+
+    <p>Partai Id: <b>{{ idPartai }}</b></p>
   </section>
 </template>
